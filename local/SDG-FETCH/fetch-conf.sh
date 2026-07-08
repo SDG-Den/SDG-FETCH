97f1157a89c841b8582e5e4e782ba263ed7a9f41
SDGDen <sdgden@sdgcloud.nl>
2026-07-08 21:05:07 +0200

    updated for test

--- local/SDG-FETCH/fetch-conf.sh
+++ local/SDG-FETCH/fetch-conf.sh
@@ -5,8 +5,8 @@ bash -c ~/.local/SDG-FETCH/update-files.sh
 SRC_DIR="$HOME/.config/SDG-FETCH/gen"
 CONF_DIR="$HOME/.config/SDG-FETCH/conf"
 
-SELECTEDLOGO=$(ls -1 $SRC_DIR | fzf --layout=reverse --preview-window=right:70% --prompt='Select a logo: ' --preview-label='Preview:' --preview='bat ~/.config/sdgos/fastfetch/gen/{}')
+SELECTEDLOGO=$(ls -1 $SRC_DIR | fzf --layout=reverse --preview-window=right:70% --prompt='Select a logo: ' --preview-label='Preview:' --preview='bat ~/.config/SDG-FETCH/gen/{}')
 
-SELECTEDCONF=$(ls -1 $CONF_DIR | fzf --layout=reverse --preview-window=down:70%,nowrap --color='pointer:green,marker:green' --prompt='Select a config style: ' --preview-label='Preview:' --preview='bash -c "fastfetch --disable-linewrap 1 -l none -c ~/.config/sdgos/fastfetch/conf/{}"')
+SELECTEDCONF=$(ls -1 $CONF_DIR | fzf --layout=reverse --preview-window=down:70%,nowrap --color='pointer:green,marker:green' --prompt='Select a config style: ' --preview-label='Preview:' --preview='bash -c "fastfetch --disable-linewrap 1 -l none -c ~/.config/SDG-FETCH/conf/{}"')
 
 echo "$SELECTEDLOGO:$SELECTEDCONF" > ~/.config/fetch.state
