1a74cf71951990f879f1dce4ce68a5a05d0d4a55
SDGDen <sdgden@sdgcloud.nl>
2026-07-15 19:44:20 +0200

    quick push with sdgbuild

--- uninstall.sh
+++ uninstall.sh
@@ -3,6 +3,8 @@
 rm -rf $HOME/.local/SDG-FETCH
 rm -rf $HOME/.local/docs/SDG-FETCH
 rm -rf $HOME/.local/tips/SDG-FETCH
+rm -rf $HOME/.local/fetch/conf/SDG-FETCH
+rm -rf $HOME/.local/fetch/logos/SDG-FETCH
 sudo unlink /usr/bin/sdgfetch
 sudo unlink /usr/bin/sdgfetch-conf
 
--- update.sh
+++ update.sh
@@ -8,5 +8,8 @@ cp -r "$WORKDIR/local/"* "$HOME/.local/"
 rm -rf "$HOME/.local/docs/SDG-FETCH" "$HOME/.local/tips/SDG-FETCH"
 cp -r "$WORKDIR/docs/"* "$HOME/.local/docs/"
 cp -r "$WORKDIR/tips/"* "$HOME/.local/tips/"
+rm -rf $HOME/.local/fetch/conf/SDG-FETCH
+rm -rf $HOME/.local/fetch/logos/SDG-FETCH
+cp -r "$WORKDIR/other/"* "$HOME/.local/fetch"
 
 sudo ln -sf "$HOME/.local/SDG-FETCH/fetch-cli.sh" /usr/bin/sdgfetch
